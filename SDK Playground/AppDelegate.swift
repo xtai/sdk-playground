b2aeb49eaa711a90ce6fd3a4f33ed2b0df0dcec1
Xiaoyu Tai <xtai.dev@gmail.com>
2023-05-24 09:59:39 -0700

    Add ATT prompt and alternative icons.

--- SDK Playground/AppDelegate.swift
+++ SDK Playground/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AppTrackingTransparency
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
@@ -13,6 +14,38 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         // AppDelegate didFinishLaunchingWithOptions
+        // code...
+                
+        // Subscribe to didBecomeActiveNotification
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(didBecomeActiveNotification),
+            name: UIApplication.didBecomeActiveNotification,
+            object: nil)
         return true
+    }
+
+    @objc func didBecomeActiveNotification() {
+        // AppDelegate didBecomeActiveNotification
+        // code...
+
+        // AppTrackingTransparency
+        if #available(iOS 14, *) {
+            print("Checking ATT authorization")
+            ATTrackingManager.requestTrackingAuthorization { (status) in
+                switch status {
+                case .denied:
+                    print("ATT authorization status is denied")
+                case .notDetermined:
+                    print("ATT authorization status is notDetermined")
+                case .restricted:
+                    print("ATT authorization status is restricted")
+                case .authorized:
+                    print("ATT authorization status is authorized")
+                @unknown default:
+                    fatalError("Invalid ATT authorization status")
+                }
+            }
+        }
     }
 }
